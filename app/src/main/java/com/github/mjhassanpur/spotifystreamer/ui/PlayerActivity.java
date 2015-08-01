fad9ddc556973c8958b7829961c4ab886a609be6
mjhassanpur <mjhassanpur@gmail.com>
2015-08-01 10:45:17 -0700

    Set dialog activity dimens

--- app/src/main/java/com/github/mjhassanpur/spotifystreamer/ui/PlayerActivity.java
+++ app/src/main/java/com/github/mjhassanpur/spotifystreamer/ui/PlayerActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 import com.github.mjhassanpur.spotifystreamer.R;
 
@@ -14,11 +15,20 @@ public class PlayerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        if (getResources().getBoolean(R.bool.twoPane)) {
+        boolean twoPane = getResources().getBoolean(R.bool.twoPane);
+        if (twoPane) {
             setTheme(R.style.BaseThemeDialog);
         }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        if (twoPane) {
+            WindowManager.LayoutParams params = getWindow().getAttributes();
+            params.height = (int) getResources().getDimension(R.dimen.activity_dialog_height);
+            params.width = (int) getResources().getDimension(R.dimen.activity_dialog_width);
+            getWindow().setAttributes(params);
+        }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
